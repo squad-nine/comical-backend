c309c0c5d5ffe8d77ec0f339ea649c5307b0d370
chelsaed <102196325+chelsaed@users.noreply.github.com>
2022-07-26 18:12:36 -0500

    initial commit

---
+++ database/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const mongoURI = 'mongodb://localhost:27017/home'
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true
+})
+.then(instance => {
+    console.log(`connected on ${instance.connections[0].name }`)
+})
+.catch(err => console.log(`Got an error see details, ${err}`))
