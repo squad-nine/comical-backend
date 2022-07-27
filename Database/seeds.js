ab4797be2060eaaba602bc48bbb4a25cdff09142
bwennin92 <brandonmw1@hotmail.com>
2022-07-27 15:16:29 -0700

    added seeds file

---
+++ Database/seeds.js
@@ -0,0 +1,61 @@
+require('./connection')
+const ComicBook = require('../models/comicBookModel')
+
+const comicBooks = [
+    {
+    image: "https://comicvine.gamespot.com/a/uploads/scale_small/5/58993/2645776-chamber_of_chills__13_cgc_8.5.jpg",
+    issueNum: 13,
+    name: "Chamber of Chills",
+    published:'',
+    condition:'',
+    date: Number
+},
+{
+    image: "https://comicvine.gamespot.com/a/uploads/scale_small/0/4/17-1497-16-1-journey-into-mystery.jpg",
+    issueNum: 3,
+    name: "Journey into Mystery",
+    published:'',
+    condition:'',
+    date: Number
+},
+{
+    image: "https://comicvine.gamespot.com/a/uploads/scale_small/0/4/18-1433-17-1-spy-cases.jpg",
+    issueNum: 13,
+    name: "Spy Cases",
+    published:'',
+    condition:'',
+    date: Number
+  },
+  {
+    image: "https://comicvine.gamespot.com/a/uploads/scale_small/0/4/22-989-23-1-blackhawk.jpg",
+    issueNum: 57,
+    name: "Blackhawk",
+    published:'',
+    condition:'',
+    date: Number
+  },
+  {
+    image: "https://comicvine.gamespot.com/a/uploads/scale_small/0/4/30-1283-31-1-marvel-tales.jpg",
+    issueNum: 109,
+    name: "Marvel Tales",
+    published:'',
+    condition:'',
+    date: Number
+  },
+  {
+    image: "https://comicvine.gamespot.com/a/uploads/scale_small/0/4/31-1423-32-1-battle.jpg",
+    issueNum: 13,
+    name: "Battle",
+    published:'',
+    condition:'',
+    date: Number
+  },
+  {
+    image: "https://comicvine.gamespot.com/a/uploads/scale_small/0/4/32-1172-33-1-lone-ranger-the.jpg",
+    issueNum: 52,
+    name: "The Lone Ranger",
+    published:'',
+    condition:'',
+    date: Number
+  }
+]
