2de7294773fcaf266fb9eae166850084eda95452
bwennin92 <brandonmw1@hotmail.com>
2022-07-27 15:10:07 -0700

    working on controller for comic

---
+++ controllers/comicController.js
@@ -0,0 +1,42 @@
+
+const ComicBook = require('../models/comicBookModel')
+
+const index = (req, res) => {
+    ComicBook.find({}, (err, comics) =>{
+            if(err){
+                res.status(400).json(err)
+                return
+            }
+            res.json(comics)
+    })
+}
+
+const comicNew = async (req, res) => {
+    console.log(req.body);
+    let newComic = await ComicBook.create(req.body)
+    res.json(newComic)
+}
+
+const deleteComic = (req, res) =>{
+    let { id } = req.params 
+    console.log(id)
+
+    ComicBook.findById(req.params.id, (err, comic) =>{
+        if(err){
+            res.status(400).json(err)
+            return
+        }
+    })
+}
+
+
+
+
+
+module.exports = {
+    index,
+    comicNew,
+    show,
+    create,
+    deleteComic
+}
--- routes/comicRoutes.js
+++ routes/comicRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const comicCtrl = require('../controllers/comicController');
 
 router.get('/', comicCtrl.index);
-router.get('/new', comicCtrl.new);
+router.get('/new', comicCtrl.comicNew);
 router.get('/:id', comicCtrl.show);
 router.post('/', comicCtrl.create);
 
