e5da9134887a527b193a1a4e46094c9ad464e188
chelsaed <102196325+chelsaed@users.noreply.github.com>
2022-07-27 19:03:15 -0500

    Added show route in controller

---
+++ controllers/comicController.js
@@ -0,0 +1,48 @@
+
+const ComicBook = require('../models/comicBookModel')
+
+const index = (req, res) => {
+    ComicBook.find({}, (err, comics) =>{
+            if(err){
+                res.status(400).json(err)
+                return
+            }
+            res.json(comics)
+    })
+}
+
+const comicNew = async (req, res) => {
+    console.log(req.body);
+    let newComic = await ComicBook.create(req.body)
+    res.json(newComic)
+}
+
+const deleteComic = (req, res) =>{
+    let { id } = req.params 
+    console.log(id)
+
+    ComicBook.findByIdAndDelete(req.params.id, (err, deletedComic) =>{
+        if(err){
+            res.status(400).json(err)
+            return
+        }
+        res.json(deletedComic)
+    })
+}
+
+const showComic = (req,res) => {
+    ComicBook.findById(req.params.id, (err,comic) => {
+        if(err){
+            res.status(400).json(err)
+            return
+        }
+    })
+}
+
+
+module.exports = {
+    index,
+    comicNew,
+    deleteComic,
+    showComic
+}
