cf78091143b8a6ee7549862d0b01716aea2a9cec
bwennin92 <brandonmw1@hotmail.com>
2022-07-29 17:21:01 -0700

    added image and issueNum to comicModel

--- models/comicBookModel.js
+++ models/comicBookModel.js
@@ -1,6 +1,8 @@
 const mongoose = require ('mongoose')
 
 const comicBook = new mongoose.Schema({
+    image: String,
+    issueNum: Number,
     name: String,
     published: String,
     condition: String,
