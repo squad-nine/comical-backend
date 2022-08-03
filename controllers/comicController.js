3a8a02ec6172a548bc562113d59e0b791fe39f71
bwennin92 <brandonmw1@hotmail.com>
2022-08-02 21:12:34 -0700

    changed create route to pull from API

--- controllers/comicController.js
+++ controllers/comicController.js
@@ -1,61 +1,80 @@
-
-const ComicBook = require('../models/comicBookModel')
+const ComicBook = require("../models/comicBookModel");
+const axios = require("axios");
 
 const index = (req, res) => {
-    ComicBook.find({}, (err, comics) =>{
-            if(err){
-                res.status(400).json(err)
-                return
-            }
-            res.json(comics)
-    })
-}
+  ComicBook.find({}, (err, comics) => {
+    if (err) {
+      res.status(400).json(err);
+      return;
+    }
+    res.json(comics);
+  });
+};
 
 const comicNew = async (req, res) => {
-    console.log(req.body);
-    let newComic = await ComicBook.create(req.body)
-    res.json(newComic)
-}
+  console.log(req.body);
+  const { name, issueNum } = req.body;
 
-const deleteComic = (req, res) =>{
-    let { id } = req.params 
-    console.log(id)
-
-    ComicBook.findByIdAndDelete(req.params.id, (err, deletedComic) =>{
-        if(err){
-            res.status(400).json(err)
-            return
-        }
-        res.json(deletedComic)
+  axios
+    .get("https://comicvine.gamespot.com/api/issues/", {
+      params: new URLSearchParams({
+        field_list: "image,site_detail_url",
+        format: "json",
+        filter: `name:${name}`,
+        issue_number: issueNum,
+      }),
     })
-}
+    .then(({ data }) => {
+      const { image, site_detail_url } = data.results[0];
+      const { small_url } = image;
+      let newComic = await ComicBook.create({image: small_url, siteUrl: site_detail_url, ...req.body});
+      res.json(newComic);
+    });
+};
 
-const showComic = (req,res) => {
-    ComicBook.findById(req.params.id, (err,comic) => {
-        if(err){
-            res.status(400).json(err)
-            return
-        } else {
-            res.json(comic)
-        }
-    })
-}
+const deleteComic = (req, res) => {
+  let { id } = req.params;
+  console.log(id);
 
-let update = (req,res) => {
-    ComicBook.findByIdAndUpdate(req.params.id,{$set:req.body},{new: true}, (err,comic) =>{
-        if(err){
-            res.status(400).json(err)
-            return
-        }
-        res.json(comic)
-    })
-}
+  ComicBook.findByIdAndDelete(req.params.id, (err, deletedComic) => {
+    if (err) {
+      res.status(400).json(err);
+      return;
+    }
+    res.json(deletedComic);
+  });
+};
 
+const showComic = (req, res) => {
+  ComicBook.findById(req.params.id, (err, comic) => {
+    if (err) {
+      res.status(400).json(err);
+      return;
+    } else {
+      res.json(comic);
+    }
+  });
+};
+
+let update = (req, res) => {
+  ComicBook.findByIdAndUpdate(
+    req.params.id,
+    { $set: req.body },
+    { new: true },
+    (err, comic) => {
+      if (err) {
+        res.status(400).json(err);
+        return;
+      }
+      res.json(comic);
+    }
+  );
+};
 
 module.exports = {
-    index,
-    comicNew,
-    deleteComic,
-    showComic,
-    update
-}
+  index,
+  comicNew,
+  deleteComic,
+  showComic,
+  update,
+};
--- routes/api/comicRoutes.js
+++ routes/api/comicRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const comicCtrl = require("../../controllers/comicController");
 
 router.get("/", comicCtrl.index);
