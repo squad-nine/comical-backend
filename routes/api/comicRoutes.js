64477c8071cc271e155a59b95660e14531182bab
bwennin92 <brandonmw1@hotmail.com>
2022-08-03 19:31:33 -0700

    added guard route to the top of controllerroute

--- routes/api/comicRoutes.js
+++ routes/api/comicRoutes.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const comicCtrl = require("../../controllers/comicController");
-
+router.use(require("../../database/auth"));
+router.use(checkAuth)
 router.get("/", comicCtrl.index);
 router.get("/new", comicCtrl.comicNew);
 router.get("/:id", comicCtrl.showComic);
 router.patch('/:id', comicCtrl.update)
 router.delete("/:id", comicCtrl.deleteComic);
+router.post("/", comicCtrl.comicNew);
 
-router.use(require("../../database/auth"));
-router.post("/", checkAuth, comicCtrl.comicNew);
 
 function checkAuth(req, res, next) {
   if (req.user) return next();
