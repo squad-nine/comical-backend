27d80d14b0b887d3ed0f9bad59fae5bf1ac7dba6
bwennin92 <brandonmw1@hotmail.com>
2022-08-03 20:13:03 -0700

    added ref profile comicmodel

--- models/comicBookModel.js
+++ models/comicBookModel.js
@@ -7,6 +7,10 @@ const comicBook = new mongoose.Schema({
   condition: String,
   date: Number,
   siteUrl: String,
+  owner:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Profile"
+  }]
 });
 const ComicBook = mongoose.model("ComicBook", comicBook);
 module.exports = ComicBook;
